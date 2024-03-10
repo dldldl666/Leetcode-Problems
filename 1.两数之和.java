a39ba5e685e4a02618ff9ddfcf3f571751289d66
Rick <1042777309@qq.com>
2024-03-10 13:34:32 +0800

    faster version of problem 1, using hashmap

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -5,19 +5,20 @@
  */
 
 // @lc code=start
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;
-                }
-            }
+    Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++){
+        if (hashtable.containsKey(target - nums[i])){
+            return new int[]{hashtable.get(target - nums[i]), i};
         }
-        return ans;
+        hashtable.put(nums[i], i);
+    }
+    return new int[0];
     }
 }
 // @lc code=end
